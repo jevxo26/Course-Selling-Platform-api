6bc06ea54870572fb6237543f3658f10c5d384f6
Aftab Farhan <aftabfarhan324@gmail.com>
2026-05-17 23:49:18 +0600

    fxied error

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,6 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // ✅ CORS FIX (এটাই missing ছিল)
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'https://course-selling-platform-five.vercel.app', // production হলে এটা লাগবে
-    ],
-    credentials: true,
-  });
-
   // Serve static files (CDN)
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
@@ -37,10 +27,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  const port = process.env.PORT || 5001;
+  const port = 5001;
   await app.listen(port);
-
   console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
